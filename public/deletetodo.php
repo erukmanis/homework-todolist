2a538a40fb8f04b247238f5cda8f62ced833e33d
eru eru <erukmanis@yahoo.com>
2019-12-14 15:58:11 +0200

    test1

---
+++ public/deletetodo.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../source/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $taskid = $_POST['delete'];
+    $stmt = $conn->prepare("DELETE FROM `schedule` WHERE `schedule`.`idschedule` = (:tasknum)");
+    $stmt->bindParam(':tasknum', $taskid);
+    $stmt->execute();
+    header('Location: /');
+} else {
+    echo "That was not a POSs, most likely GET";
+}
